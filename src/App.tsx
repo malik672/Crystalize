9213c3460864e78a547d7589671a24ee93c7a2ae
malik672 <aremumalik01@gamil.com>
2022-06-23 04:57:51 +0100

    commit in submodule

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const[state, setState] = useState("");
+  useEffect(() => {
+    const home = (async() => {
+      const data =  await fetch("https://api.nasa.gov/planetary/apod?api_key=P7uoXt969xdSvSsNXAVvuVSZ5lWiU6UiMUWTWuSL&date=2014-10-01&concept_tags=True");
+      const url = data.json();
+      const imgs = await url;
+      console.log(imgs.url);
+      setState(imgs.url);
+    })
+    home();
+  })
+  
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Fetch nasa image of the day using nasa api</h1>
+        <img src={state}></img>
       </header>
     </div>
   );
